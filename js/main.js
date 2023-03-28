2713602f98db31f06839441f1f55fec88b288167
Nadja Tur <nadjatur1999@gmail.com>
2023-03-28 20:58:59 +0200

    added js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 function myFunction() {
     document.getElementById("dropdown-content").classList.toggle("show");
   }
+
+  function photoOne(){
+    document.getElementById('hero_im').style.backgroundImage = "url('assets/920.png')";
+    document.getElementById('hero_im').style.backgroundSize= "cover";
+  }
+
+  function photoTwo(){
+    document.getElementById('hero_im').style.backgroundImage = "url('assets/17657126.png')";
+    document.getElementById('hero_im').style.backgroundSize= "cover";
+  }
+
+  function photoThree(){
+    document.getElementById('hero_im').style.backgroundImage = "url('assets/3118774.png')";
+    document.getElementById('hero_im').style.backgroundSize= "cover";
+  }
+
+  function photoFour(){
+    document.getElementById('hero_im').style.backgroundImage= "url('assets/Cezanne-Stylign-Line.png')";
+    document.getElementById('hero_im').style.backgroundSize= "cover";
+  }
